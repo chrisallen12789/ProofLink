fbf3c295499948df78f4c36ba04d69f7ab9b966f
Christopher Lane <75538282+chrisallen12789@users.noreply.github.com>
2026-03-30 13:04:54 -0400

    Create load-env.ps1 script to load environment variables for PowerShell

---
+++ load-env.ps1
@@ -0,0 +1,26 @@
+# Load-Env PowerShell Script
+
+param (
+    [string]$envFile = '.env.local'
+)
+
+# Check if the specified .env file exists, if not, fallback to .env.example
+if (-Not (Test-Path $envFile)) {
+    $envFile = '.env.example'
+}
+
+# Check if the envFile exists after fallback
+if (-Not (Test-Path $envFile)) {
+    Write-Error "The specified .env file '$envFile' does not exist."
+    return
+}
+
+# Load environment variables from the .env file
+Get-Content $envFile | ForEach-Object {
+    if ($_.Contains('=') -and -not $_.StartsWith('#')) {
+        $name, $value = $_ -split '=';
+        [System.Environment]::SetEnvironmentVariable($name.Trim(), $value.Trim());
+    }
+}
+
+Write-Output "Environment variables loaded from $envFile successfully!"
